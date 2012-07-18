99ed58b9ff3aca2b8dddd4330853df006f05c1df
ethemcem.ozkan@gmail.com <ethemcem.ozkan@gmail.com@b6144a0a-d1a6-8e64-4b95-5ef747909e12>
2012-07-18 08:26:36 +0000

    Properties classında ekleme yapıldı

--- src/java/gov/tubitak/ikis/service/Properties.java
+++ src/java/gov/tubitak/ikis/service/Properties.java
@@ -4,6 +4,14 @@
  */
 package gov.tubitak.ikis.service;
 
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
+import gov.tubitak.ikis.database.Sparql;
+import java.util.Iterator;
+import java.util.List;
+import java.lang.String;
+
 
 
 /**
@@ -13,13 +21,31 @@ package gov.tubitak.ikis.service;
 public class Properties {
     public static String[] getHeaders(){
         String[] array = null;
-        String query="select ?name where{?pro rdf:type owl:ObjectProperty. :}";
+        String query="select ?pro where{?pro rdf:type owl:ObjectProperty. ?pro :propertyType "+"header"+"}";
+        ResultSet search = Sparql.search(query);
+        List<QuerySolution> toList = ResultSetFormatter.toList(search);
+        array=new String[toList.size()];
+        Iterator<QuerySolution> iterator = toList.iterator();
+        int i=0;
+        while (iterator.hasNext()) {
+            QuerySolution next = iterator.next();
+            array[i]=next.get("pro").toString();
+        }
         return array;
     }
     
     public static String[] getMetadata(String header){
         String[] array = null;
-        String query="select ?name where{?pro rdf:type owl:ObjectProperty. ?pro owl:subPropertyOf ?name}";
+        String query="select ?pro where{?pro rdf:type owl:ObjectProperty. ?pro owl:subPropertyOf "+header+".}";
+        ResultSet search = Sparql.search(query);
+        List<QuerySolution> toList = ResultSetFormatter.toList(search);
+        array=new String[toList.size()];
+        Iterator<QuerySolution> iterator = toList.iterator();
+        int i=0;
+        while (iterator.hasNext()) {
+            QuerySolution next = iterator.next();
+            array[i]=next.get("pro").toString();
+        }
         return array;
     }
 }

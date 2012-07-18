5b828f6eaaa812f15aa6d251fc56857cdb8e573d
ethemcem.ozkan@gmail.com <ethemcem.ozkan@gmail.com@b6144a0a-d1a6-8e64-4b95-5ef747909e12>
2012-07-18 08:56:49 +0000

    data sınıfları eklendi

--- trunk/src/java/gov/tubitak/ikis/items/Data.java
+++ trunk/src/java/gov/tubitak/ikis/items/Data.java
@@ -10,4 +10,33 @@ package gov.tubitak.ikis.items;
  */
 public class Data {
     
+    private String value;
+    private Item belongsTo;
+    private Property property;
+
+    public Item getBelongsTo() {
+        return belongsTo;
+    }
+
+    public void setBelongsTo(Item belongsTo) {
+        this.belongsTo = belongsTo;
+    }
+
+    public Property getProperty() {
+        return property;
+    }
+
+    public void setProperty(Property property) {
+        this.property = property;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+    
+    
 }

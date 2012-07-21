52ec65bc71d6d7cdfc48ac3f6449b48d5e3dad3d
ethemcem.ozkan@gmail.com <ethemcem.ozkan@gmail.com@b6144a0a-d1a6-8e64-4b95-5ef747909e12>
2012-07-21 15:59:23 +0000

    methodlar düzenlendi

--- src/java/gov/tubitak/ikis/beans/PageBean.java
+++ src/java/gov/tubitak/ikis/beans/PageBean.java
@@ -4,7 +4,12 @@
  */
 package gov.tubitak.ikis.beans;
 
+import gov.tubitak.ikis.items.Data;
 import gov.tubitak.ikis.items.Item;
+import gov.tubitak.ikis.items.Stage1;
+import gov.tubitak.ikis.items.Stage2;
+import gov.tubitak.ikis.service.DataFinder;
+import gov.tubitak.ikis.service.GetItems;
 import gov.tubitak.ikis.service.Properties;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
@@ -18,17 +23,75 @@ import javax.faces.bean.ViewScoped;
 @ViewScoped
 public class PageBean {
     
-    private List<Item> selectedHeaders;
+    private Item[] selectedHeaders;
+    private Item[] selectedMetadatas;
+    private Stage1[] selectedStage1;
+    private Stage2[] selectedStage2;
+    private Item[] selectedProvince;
 
-    public void setSelectedHeaders(List<Item> selectedHeaders) {
+    public Item[] getSelectedMetadatas() {
+        return selectedMetadatas;
+    }
+
+    public void setSelectedMetadatas(Item[] selectedMetadatas) {
+        this.selectedMetadatas = selectedMetadatas;
+    }
+
+    public Item[] getSelectedProvince() {
+        return selectedProvince;
+    }
+
+    public void setSelectedProvince(Item[] selectedProvince) {
+        this.selectedProvince = selectedProvince;
+    }
+
+    public Item[] getSelectedStage1() {
+        return selectedStage1;
+    }
+
+    public void setSelectedStage1(Stage1[] selectedStage1) {
+        this.selectedStage1 = selectedStage1;
+    }
+
+    public Item[] getSelectedStage2() {
+        return selectedStage2;
+    }
+
+    public void setSelectedStage2(Stage2[] selectedStage2) {
+        this.selectedStage2 = selectedStage2;
+    }
+
+    public void setSelectedHeaders(Item[] selectedHeaders) {
         this.selectedHeaders = selectedHeaders;
     }
 
-    public List<Item> getSelectedHeaders() {
+    public Item[] getSelectedHeaders() {
         return selectedHeaders;
     }
     
     public Item[] getHeaders(){
         return Properties.getHeaders();
+    }
+    
+    public Item[] getStage1(){
+        return GetItems.getStage1();
+    }
+    
+    public Item[] getStage2(){
+        return GetItems.getStage2(selectedStage1[0]);
+    }
+    
+    public Item[] getProvince(){
+        return GetItems.getProvince(selectedStage2[0]);
+    }
+    
+    public Item[] getMetadatas(){
+        if(selectedHeaders.length>0)
+            return Properties.getMetadata(selectedHeaders[0].getName());
+        return null;
+    }
+    
+    public Data[] getDatas(){
+        return DataFinder.getdata(selectedHeaders); //TODO Düzgün parametre ver
     }
 }

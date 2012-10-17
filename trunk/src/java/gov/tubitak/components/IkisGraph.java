6249f5ac23e395578c58f7fc157a5f65106d9b3e
ethemcem.ozkan@gmail.com <ethemcem.ozkan@gmail.com@b6144a0a-d1a6-8e64-4b95-5ef747909e12>
2012-10-17 12:59:55 +0000

    bileşene baslandı

--- trunk/src/java/gov/tubitak/components/IkisGraph.java
+++ trunk/src/java/gov/tubitak/components/IkisGraph.java
@@ -5,12 +5,17 @@
 package gov.tubitak.components;
 
 import gov.tubitak.components.models.GraphModel;
+import java.io.IOException;
+import javax.faces.component.FacesComponent;
 import javax.faces.component.UIComponentBase;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
 
 /**
  *
  * @author cem.ozkan
  */
+@FacesComponent(value="IkisGraph")
 public class IkisGraph extends UIComponentBase{
     private GraphModel submittedValue;
     
@@ -26,5 +31,25 @@ public class IkisGraph extends UIComponentBase{
     public void setSubmittedValue(GraphModel submittedValue) {
         this.submittedValue = submittedValue;
     }
+
+    @Override
+    public void encodeEnd(FacesContext context) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+        writer.startElement("div", this);
+        writer.startElement("script", null);
+        writer.append("var g = new Graph();");
+        writer.append("g.addNode(\"id34\", { label : \"Duzey1\" });");
+         writer.append("g.addNode(\"id35\", { label : \"Duzey2\" });");
+         writer.append("g.addNode(\"id36\", { label : \"Il\" });");
+         writer.append("g.addEdge(\"id35\", \"id34\", { stroke : \"#bfa\" , fill : \"#56f\", label : \"hasRegion\", directed : true});");
+         writer.append("g.addEdge(\"id35\", \"id36\", { stroke : \"#bfa\" , fill : \"#56f\", label : \"hasCity\", directed : true});");
+         writer.append("var layouter = new Graph.Layout.Spring(g);");
+         writer.append("layouter.layout();");
+         writer.append("var renderer = new Graph.Renderer.Raphael('canvas', g, '400', '300');");
+         writer.append("renderer.draw();");
+        writer.endElement("script");
+        writer.endElement("div");
+    }
+    
     
 }
--- trunk/src/java/gov/tubitak/components/models/GraphModel.java
+++ trunk/src/java/gov/tubitak/components/models/GraphModel.java
@@ -1,13 +1,35 @@
 /*
- * To change this template, choose Tools | Templates
+ * Stringo change this template, choose Stringools | Stringemplates
  * and open the template in the editor.
  */
 package gov.tubitak.components.models;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author cem.ozkan
  */
 public class GraphModel{
+    private List<String[]> list;
+
+    public GraphModel() {
+        list=new ArrayList<String[]>();
+    }
     
+    public void addStringriple(String object, String property, String subject){
+        String[] array = new String[3];
+        array[0]=object;
+        array[1]=property;
+        array[2]=subject;
+        list.add(array);
+    }
+    
+    public String[] getTriple(int i){
+        return list.get(i);
+    }
+    public List<String[]> getTriples(){
+        return list;
+    }    
 }
